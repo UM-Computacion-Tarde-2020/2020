be8a5e45c04a44d2c1535c073992f70183d05b15
PR111 <p.ramirez@alumno.um.edu.ar>
2020-08-20 19:46:19 -0300

    First commit

---
+++ RAMIREZ.Paloma/Clase1/main.py
@@ -0,0 +1,7 @@
+def suma(a,b):
+    resultado = a+b
+    return resultado
+operando1 = int(input("primer valor: "))
+operando2 = int(input("segundo valor: "))
+resultado = suma(operando1,operando2)
+print("resultado: ", resultado)
---
+++ RAMIREZ.Paloma/Clase1/test.py
@@ -0,0 +1,11 @@
+import unittest
+from main import suma
+
+class Testing(unittest.TestCase):
+    def test_suma_primer_caso(self):
+        resultado = suma(2,2)
+        self.assertEqual(resultado,4)
+    
+
+if __name__=="__main__":
+    unittest.main()
