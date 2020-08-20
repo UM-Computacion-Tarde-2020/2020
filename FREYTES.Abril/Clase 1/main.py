4e4e6c5e75479621845d62e3118f960a207d0676
AbiFreytes <a.freytes@alumno.um.edu.com>
2020-08-20 19:48:31 -0300

    Clase 1

---
+++ FREYTES.Abril/Clase 1/main.py
@@ -0,0 +1,13 @@
+print('hola mundo')
+
+
+def suma(a, b):
+    resultado = a + b
+    return resultado
+
+
+if __name__ == '__main__':
+    operando1 = int(input("ingrese un valor: "))
+    operando2 = int(input("ingrese otro valor: "))
+    resultado = operando1 + operando2
+    print("Resultado: ", resultado)
