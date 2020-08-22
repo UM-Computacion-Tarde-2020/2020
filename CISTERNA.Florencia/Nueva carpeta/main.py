ab7fce827328a16506a864562f8bb437dba6ab94
sofii2020 <soficareinoso@gmail.com>
2020-08-22 16:56:37 -0300

    First commit 20/09

--- CISTERNA.Florencia/Nueva carpeta/main.py
+++
@@ -1,5 +0,0 @@
-def(operando1,operando2):
-    resultado = operando1+operando2
-    return resultado
-operando1=int(input('ingrese un valor')
-operando2= int(input('ingrese otro valor'))
--- RAMIREZ.Paloma/Clase1/main.py
+++
@@ -1,7 +0,0 @@
-def suma(a,b):
-    resultado = a+b
-    return resultado
-operando1 = int(input("primer valor: "))
-operando2 = int(input("segundo valor: "))
-resultado = suma(operando1,operando2)
-print("resultado: ", resultado)
