7cb3dd2fdbe1b8ce62119a134bc3a3c7d39e4007
rociobustos332 <rb.bustos@alumno.um.edu.ar>
2020-08-25 10:20:05 -0300

    First commit

---
+++ BUSTOS.Rocio/Clase1-20-08-20/main.py
@@ -0,0 +1,10 @@
+def suma(a, b):
+    resultado = a + b
+    return resultado
+
+
+if __name__ == '__main__':
+    operando1 = int(input("Ingrese un valor:"))
+    operando2 = int(input("Ingrese otro valor:"))
+    resultado = suma(operando1, operando2)
+    print("Resultado", resultado)
