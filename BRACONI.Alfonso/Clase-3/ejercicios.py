6bbf67f474fc7be1c8f392e28cbbd09c3c62a687
AlfonsoBraconi <alfonsobraconi2000@gmail.com>
2020-09-03 19:52:25 -0300

    Upload files class 20-09-03

---
+++ BRACONI.Alfonso/Clase-3/ejercicios.py
@@ -0,0 +1,25 @@
+def mes_a_letras(number):
+    if number == 1:
+        return "Enero"
+    if number == 2:
+        return "Febrero"
+    if number == 3:
+        return "Marzo"
+    if number == 4:
+        return "Abril"
+    if number == 5:
+        return "Mayo"
+    if number == 6:
+        return "Junio"
+    if number == 7:
+        return "Julio"
+    if number == 8:
+        return "Agosto"
+    if number == 9:
+        return "Septiembre"
+    if number == 10:
+        return "Octubre"
+    if number == 11:
+        return "Noviembre"
+    if number == 12:
+        return "Diciembre"
