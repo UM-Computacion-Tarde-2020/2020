aa1fff209224b49c7e2f1a5626cd13946ce378b6
Zuca07 <g.zucarelli@alumno.um.edu.ar>
2020-09-03 20:26:50 -0300

    clase3 3/09

--- ZUCARELLI.gabriel/clase3/ejercicios.py
+++ ZUCARELLI.gabriel/clase3/ejercicios.py
@@ -5,4 +5,4 @@ def mes_a_letras(mes):        #las funciones comienzan con "def". Además, "mes"
     #busco la posicion del  numero dentro de la lista
     nombre_mes = meses[mes -1] #resto 1 porque comienza a contar desde 0. El mes Enero es el mes 0, y Febrero sería el mes 2
     #devuelvo el valor encontrado
-    return nombre_mes #se escribe return para que la función devuleva un valor
+    return nombre_mes #se escribe return para que la función devulva un valor
