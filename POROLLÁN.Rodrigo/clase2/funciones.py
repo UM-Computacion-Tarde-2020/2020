ea8695baec9d6332c2c68cc3d55653defedacd54
RodriPoro <rodriporollan@gmail.com>
2020-09-03 19:52:29 -0300

    Upload files clase 3/9/20

---
+++ POROLLÁN.Rodrigo/clase2/funciones.py
@@ -0,0 +1,12 @@
+def funcion_que_hace_algo ():
+    return "Hola mundo"
+
+def funcion_que_hace_otra_cosa():
+    return 145.5    
+
+def funcion(a):
+    
+    lista = []    
+    for i in range( 1, a + 1):
+        lista.append(i)
+    return (lista)    
---
+++ POROLLÁN.Rodrigo/clase3/ejercicios1.py
@@ -0,0 +1,12 @@
+def mes_a_letras (a):
+    mes = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 
+    'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
+    a = (mes[a-1]) 
+   
+    return a
+
+
+
+
+
+        
