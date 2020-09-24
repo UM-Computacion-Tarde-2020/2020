5474ff11dc9d1ca7c119e07e53fc8fa509e89297
AbiFreytes <a.freytes@alumno.um.edu.com>
2020-09-24 19:59:46 -0300

    Clase 4

---
+++ FREYTES.Abril/Clase 4/test.py
@@ -0,0 +1,20 @@
+import unittest
+from main import split
+
+
+class Testing(unittest.TestCase):
+    def test_extraer_impares(self):
+        list = [i for i in range(0, 100)]
+        print(list)
+        list_test = split(list)
+        list_impar = [i for i in range(1, 100, 2)]
+        self.assertEqual(list_test, list_impar)
+
+    def test_otros_impares(self):
+        list = [1, 4, 5, 8, 11, 16]
+        list_test = split(list)
+        self.assertEqual(list_test, [1, 5, 11])
+
+
+if __name__ == '__main__':
+    unittest.main()
